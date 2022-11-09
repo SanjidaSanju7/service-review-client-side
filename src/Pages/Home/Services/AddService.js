f4bae021a5d6d97570089d1bcadb0b2cab2f7b5a
SanjidaSanju7 <sanjidaakter6789@gmail.com>
2022-11-09 17:29:46 +0600

    add service created

--- src/Pages/Home/Services/AddService.js
+++ src/Pages/Home/Services/AddService.js
@@ -1,10 +1,56 @@
 import React from 'react';
 
 const AddService = () => {
-    return (
-        <div>
 
-        </div>
+    const handleAddService = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const price = form.price.value;
+        const description = form.description.value;
+        const image = form.image.value;
+
+
+
+        const services = {
+            name,
+            price,
+            description,
+            image
+        }
+
+        fetch('http://localhost:5000/services', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(services)
+        })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
+
+
+
+    }
+
+    return (
+        <section className="p-6 dark:bg-gray-800 dark:text-gray-50">
+            <h2 className='text-4xl text-purple-500 font-serif'>Add a Service</h2>
+            <form onSubmit={handleAddService} noValidate="" action="" className="container flex flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid bg-purple-100">
+                <fieldset className="gap-6 p-6 rounded-md shadow-sm dark:bg-gray-900">
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                        <input name="name" type="text" placeholder="Service Name" className="input input-bordered w-full" />
+                        <input name="price" type="text" placeholder="Service Price" className="input input-bordered w-full " />
+                        <input name="image" type="text" placeholder="ImageURL" className="input input-bordered w-full" />
+                        <textarea name="description" type="text" className="textarea textarea-bordered p-10" placeholder="Service Description"></textarea>
+                    </div>
+                    <button className="btn mx-auto bg-purple-500 mt-5 w-1/4">Add Service</button>
+                </fieldset>
+
+
+            </form>
+        </section>
     );
 };
 
